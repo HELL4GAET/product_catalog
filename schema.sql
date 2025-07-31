b9b76bc8a16ce4e25b73c9f94ca64287b3b82be5
Cherkez Aleksandr <cherkezalexandr@gmail.com>
2025-07-31 12:35:04 +0300

    added sql schema (creating tables users and product)

---
+++ schema.sql
@@ -0,0 +1,21 @@
+-- Таблица карточки продукта
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    price INTEGER NOT NULL CHECK (price > 0),
+    available BOOLEAN NOT NULL DEFAULT TRUE,
+    description TEXT,
+    image_url TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+)
+
+-- Таблица пользователей
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'user',
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+)
+
