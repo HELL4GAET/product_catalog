23aa8e742ae319c87419bdc1e497d2c9c8dc52b6
Cherkez Aleksandr <cherkezalexandr@gmail.com>
2025-07-31 17:08:17 +0300

    aded user repo methods

--- internal/adapters/db/pg_user_repo.go
+++ internal/adapters/db/pg_user_repo.go
@@ -1,1 +1,70 @@
 package db
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"product-catalog/internal/user"
+)
+
+type UserRepo struct {
+	db *pgxpool.Pool
+}
+
+func NewUserRepo(db *pgxpool.Pool) *UserRepo {
+	return &UserRepo{db: db}
+}
+
+func (r *UserRepo) Create(ctx context.Context, user *user.User) error {
+	const query = `INSERT INTO users (username, email, password_hash, role, created_at) VALUES ($1, $2, $3, $4, $5)`
+	_, err := r.db.Exec(ctx, query, user.Username, user.Email, user.PasswordHash, user.Role, user.CreatedAt)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return ErrConflict
+		}
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+	return nil
+}
+
+func (r *UserRepo) GetByID(ctx context.Context, id int) (*user.User, error) {
+	const query = `SELECT id, username, email, role, created_at FROM users WHERE id = $1`
+	var userFromDB user.User
+	err := r.db.QueryRow(ctx, query, id).Scan(&userFromDB.ID, &userFromDB.Username, &userFromDB.Email, &userFromDB.Role, &userFromDB.CreatedAt)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, fmt.Errorf("failed to get user by id: %w", err)
+	}
+	return &userFromDB, nil
+}
+
+func (r *UserRepo) UpdateByID(ctx context.Context, id int, user *user.User) error {
+	const query = `UPDATE users SET username = $1, email = $2, role = $3 WHERE id = $4`
+	_, err := r.db.Exec(ctx, query, user.Username, user.Email, user.Role, id)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return ErrConflict
+		}
+		return fmt.Errorf("failed to update user: %w", err)
+	}
+	return nil
+}
+
+func (r *UserRepo) DeleteByID(ctx context.Context, id int) error {
+	const query = `DELETE FROM users WHERE id = $1`
+	_, err := r.db.Exec(ctx, query, id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return ErrNotFound
+		}
+		return fmt.Errorf("failed to delete user: %w", err)
+	}
+	return nil
+}
