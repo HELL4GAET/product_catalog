1a77f3c60c0f028e2401ad8fdb42a5e9b01bc23f
Cherkez Aleksandr <cherkezalexandr@gmail.com>
2025-08-01 21:58:30 +0300

    added product dto

--- internal/dto/product_dto.go
+++ internal/dto/product_dto.go
@@ -1,4 +1,9 @@
 package dto
 
 type CreateProductInput struct {
+	Title       string `validate:"required,min=3,max=12"`
+	Price       int    `validate:"required,min=1,max=100000"`
+	Description string `validate:"required,min=3,max=120"`
+	Available   bool   `validate:"required"`
+	ImageURL    string `validate:"required,url"`
 }
