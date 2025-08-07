e389e24aee95df1b822427c5511f9a36cdb790dd
Cherkez Aleksandr <cherkezalexandr@gmail.com>
2025-08-07 15:00:17 +0300

    refacrot product dto

--- internal/dto/product_dto.go
+++ internal/dto/product_dto.go
@@ -4,6 +4,14 @@ type CreateProductInput struct {
 	Title       string `validate:"required,min=3,max=12"`
 	Price       int    `validate:"required,min=1,max=100000"`
 	Description string `validate:"required,min=3,max=120"`
-	Available   bool   `validate:"required"`
+	Available   bool
 	ImageURL    string `validate:"required,url"`
+}
+
+type UpdateProductInput struct {
+	Title       *string `validate:"min=3,max=12"`
+	Price       *int    `validate:"min=1,max=100000"`
+	Description *string `validate:"min=3,max=120"`
+	Available   *bool   `validate:"required"`
+	ImageURL    *string
 }
