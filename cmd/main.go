649454b24ff4e1090fa8a4989480d92d807bcccc
Cherkez Aleksandr <cherkezalexandr@gmail.com>
2025-08-12 14:30:12 +0300

    refactor openapi spec

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"go.uber.org/zap"
 	"log"
 	"net/http"
@@ -32,9 +33,23 @@ func main() {
 	r.Mount("/users", d.UserHandler.Routes())
 	r.Mount("/products", d.ProductHandler.Routes())
 
-	workDir, _ := os.Getwd()
-	docsPath := filepath.Join(workDir, "docs")
-	r.Handle("/docs/*", http.StripPrefix("/docs/", http.FileServer(http.Dir(docsPath))))
+	r.Route("/docs", func(r chi.Router) {
+		r.Use(cors.Handler(cors.Options{
+			AllowedOrigins:   []string{"http://localhost:8080"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: true,
+			MaxAge:           300,
+		}))
+		workDir, _ := os.Getwd()
+		docsPath := filepath.Join(workDir, "docs")
+		r.Handle("/*", http.StripPrefix("/docs/", http.FileServer(http.Dir(docsPath))))
+	})
+
+	//workDir, _ := os.Getwd()
+	//docsPath := filepath.Join(workDir, "docs")
+	//r.Handle("/docs/*", http.StripPrefix("/docs/", http.FileServer(http.Dir(docsPath))))
 
 	addr := ":" + cfg.Server.Port
 	d.Logger.Info("server started", zap.String("addr", addr))
